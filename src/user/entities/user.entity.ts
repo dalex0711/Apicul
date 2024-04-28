4c90dffacc07389883e72d79eb28ff6ba326553c
dalexa <dalexsanjuan@outlook.com>
2024-04-28 17:11:39 -0500

    Creación de recursos User

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    firstName: string;
+  
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    phonenumber: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+    
+}
