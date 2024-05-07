244daad027450e3e1d442475a50679c5234adf22
dalexa <dalexsanjuan@outlook.com>
2024-05-06 20:46:17 -0500

    Creando Repository para User: 5 metodos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/entities/User';
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
